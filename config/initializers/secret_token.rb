dfec3b8c07a64544855e97eb365fa9af9cebcc41
cpl <cp@imac-office.home>
2013-02-15 16:38:59 -0500

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'f4fc4a317bca5ebb8fe8d617d654ace3cb989a05ad6dd0619713b16a0ba71727c460f4db1487a1a62faa4caa0fe0d14f6dc96a5936c3b2032ff052eb2d07aa1b'
